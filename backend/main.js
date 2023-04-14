dc695128b96996454a7182605b0bc39f30b43339
Eduardo Fernandes <edutf76@gmail.com>
2023-04-15 08:43:36 +1000

    Adiconando codigo do backend

---
+++ backend/main.js
@@ -0,0 +1,13 @@
+console.log('Iniciando servidor do Ecomuseu');
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000);
+
+console.log('http://localhost:3000');
+console.log('Servidor Ecomuseu pronto e operacional.')
