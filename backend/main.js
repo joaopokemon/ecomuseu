0e4da00d9c5acafb5daffdc59222d35c3fb1de38
Eduardo Fernandes <edutf76@gmail.com>
2023-04-15 09:02:08 +1000

    Alteracoes no servidor para incluir paginas html

--- backend/main.js
+++ backend/main.js
@@ -1,13 +1,17 @@
+const express = require('express')
+
+
 console.log('Iniciando servidor do Ecomuseu');
 
-const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+app.use('/', express.static('www'));
+
+app.get('/api', function (req, res) {
   res.send('Hello World')
 })
 
 app.listen(3000);
 
-console.log('http://localhost:3000');
+console.log('\n\nPara acessar o Ecomuseu, va para a URL http://localhost:3000\n\n');
 console.log('Servidor Ecomuseu pronto e operacional.')
